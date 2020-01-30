c58d50a2e09382268782289dc6593bf6cefcdf6b
hyobum <hyob1203@naver.com>
2020-01-30 22:52:40 +0900

    add svelte-preprocess

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import config from './svelte.config'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -15,15 +16,7 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
+		svelte(config),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
